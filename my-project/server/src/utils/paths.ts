08b39cc4bc22d4d18e472a2f4f67322d6571a9e9
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-31 16:49:50 -0400

    Create paths.ts

---
+++ my-project/server/src/utils/paths.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const messagesFilePath = path.join(__dirname, './messages.json');
+export const usersFilePath = path.join(__dirname, './users.json');
+export const chatFilePath = path.join(__dirname, './chat.json');
